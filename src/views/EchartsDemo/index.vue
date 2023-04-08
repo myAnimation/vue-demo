cf3f07c79c78d5e713fb1865d2e2f705bc4f5546
duanyongpeng <292747553@qq.com>
2023-04-08 17:05:32 +0800

    first commit

---
+++ src/views/EchartsDemo/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="EchartsDemo">
+    <div class="echartsOne" ref="echartsOne"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EchartsDemo",
+  data() {
+    return {
+      echartsOne: null,
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.createdEchartsOne();
+    });
+  },
+  methods: {
+    createdEchartsOne() {
+      this.echartsOne = this.$echarts.init(this.$refs.echartsOne);
+      let option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            // Use axis to trigger tooltip
+            type: "shadow", // 'shadow' as default; can also be 'line' or 'shadow'
+          },
+        },
+        legend: {},
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true,
+        },
+        xAxis: {
+          type: "value",
+        },
+        yAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        series: [
+          {
+            name: "Direct",
+            type: "bar",
+            stack: "total",
+            label: {
+              show: true,
+            },
+            emphasis: {
+              focus: "series",
+            },
+            data: [320, 302, 301, 334, 390, 330, 320],
+          },
+          {
+            name: "Mail Ad",
+            type: "bar",
+            stack: "total",
+            label: {
+              show: true,
+            },
+            emphasis: {
+              focus: "series",
+            },
+            data: [120, 132, 101, 134, 90, 230, 210],
+          },
+          {
+            name: "Affiliate Ad",
+            type: "bar",
+            stack: "total",
+            label: {
+              show: true,
+            },
+            emphasis: {
+              focus: "series",
+            },
+            data: [220, 182, 191, 234, 290, 330, 310],
+          },
+          {
+            name: "Video Ad",
+            type: "bar",
+            stack: "total",
+            label: {
+              show: true,
+            },
+            emphasis: {
+              focus: "series",
+            },
+            data: [150, 212, 201, 154, 190, 330, 410],
+          },
+          {
+            name: "Search Engine",
+            type: "bar",
+            stack: "total",
+            label: {
+              show: true,
+            },
+            emphasis: {
+              focus: "series",
+            },
+            data: [820, 832, 901, 934, 1290, 1330, 1320],
+          },
+        ],
+      };
+      this.echartsOne.setOption(option);
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.EchartsDemo {
+  width: 100%;
+  min-height: calc(100vh - 50px);
+  .echartsOne {
+    width: 400px;
+    height: 400px;
+  }
+}
+</style>
